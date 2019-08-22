fba141e1b8b6e848d314d314160c42ea14c084fe
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-08-22 12:26:51 +0200

    Added while chunk.

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -11,6 +11,19 @@
 function chunk(arr, size) {
   const chunked = [];
 
+  let index = 0;
+
+  while (index < arr.length) {
+    chunked.push(arr.slice(index, index + size));
+    index += size;
+  }
+
+  return chunked;
+}
+
+function chunkForOf(arr, size) {
+  const chunked = [];
+
   for (let el of arr) {
     let last = chunked[chunked.length - 1];
     if (!last || last.length === size) {
